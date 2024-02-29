502e0f7e5406a54dc1809a3e5667acdf9e3f68a4
DeLaVega <example@mail.com>
2024-02-29 14:41:38 +0300

    The third project is the second part

--- registerInBD.py
+++ registerInBD.py
@@ -3,7 +3,7 @@ import sqlite3
 
 
 def main(page: ft.Page):
-    page.title = "Flet App"
+    page.title = "DeLaVega APP"
     page.theme_mode = 'dark'
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
@@ -23,7 +23,7 @@ def main(page: ft.Page):
             pass TEXT)"""
         )
         cur.execute(f"INSERT INTO users VALUES(NULL, '{user_login.value}', '{user_pass.value}')")
-
+        db.commit()
         db.close()
 
         user_login.value = ''
@@ -32,25 +32,59 @@ def main(page: ft.Page):
 
         page.update()
 
+    def auth_user(e):
+        db = sqlite3.connect('my.base')
+        cur = db.cursor()
+        cur.execute(f"SELECT * FROM users WHERE login = '{user_login.value}' AND pass = '{user_pass.value}'")
+        if cur.fetchone():
+            user_login.value = ''
+            user_pass.value = ''
+            btn_auth.text = 'Авторизовано'
+            page.update()
+        else:
+            page.snack_bar = ft.SnackBar(ft.Text("Неверно введенные данные"))
+            page.snack_bar.open = True
+            page.update()
+
+
+        db.commit()
+        db.close()
+        
+
     def validate(e):
         if all([user_login.value, user_pass.value]):
             btn_reg.disabled = False
+            btn_auth.disabled = False
         else:
-            btn_reg.did_mount = True
+            btn_reg.disabled = True
+            btn_auth.disabled = True
 
         page.update()
 
     user_login = ft.TextField(label='Логин', width=200, on_change=validate)
     user_pass = ft.TextField(label='Пароль', password=True, width=200, on_change=validate)
     btn_reg = ft.OutlinedButton(text='Добавить', width=200, on_click=register, disabled=True)
+    btn_auth = ft.OutlinedButton(text='Авторизовать', width=200, on_click=auth_user, disabled=True)
 
-
-    page.add(
-        ft.Row(
+    panel_auth = ft.Row(
             [
                 ft.Column(
                     [
-                        ft.Text('Регисрация'),
+                        ft.Text('Авторизация'),
+                        user_login,
+                        user_pass,
+                        btn_auth
+                    ]
+                )
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+
+    panel_register = ft.Row(
+            [
+                ft.Column(
+                    [
+                        ft.Text('Регистрация'),
                         user_login,
                         user_pass,
                         btn_reg
@@ -59,7 +93,21 @@ def main(page: ft.Page):
             ],
             alignment=ft.MainAxisAlignment.CENTER
         )
+
+    def navigate(e):
+        index = page.navigation_bar.selected_index
+        page.clean()
+        if index==0: page.add(panel_register)
+        elif index==1: page.add(panel_auth)
+
+    page.navigation_bar =ft.NavigationBar(
+        destinations=[
+            ft.NavigationDestination(icon=ft.icons.VERIFIED_USER, label="Регистрация"),
+            ft.NavigationDestination(icon=ft.icons.VERIFIED_USER_OUTLINED, label="Авторизация"),
+        ], on_change = navigate
     )
+
+    page.add(panel_register)
 
 
 #ft.app(target=main, view=ft.AppView.WEB_BROWSER)
